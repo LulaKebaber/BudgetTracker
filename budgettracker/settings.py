195f67c04ae515b6b76755147999e940bc9f558b
Danial <danial-baizak@mail.ru>
2023-11-03 16:44:27 +0100

    docker-compose, dockerfile

--- budgettracker/settings.py
+++ budgettracker/settings.py
@@ -68,19 +68,19 @@ WSGI_APPLICATION = 'budgettracker.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': "postgres",
-    #     'USER': "postgres",
-    #     'PASSWORD': "postgres",
-    #     'HOST': "localhost",
-    #     'PORT': "5432",
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',  # This should match the service name in your Docker Compose file
+        'PORT': '5432',
+    }
 }
 
 
