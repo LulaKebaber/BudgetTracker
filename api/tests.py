70be592831e9e5c6ea03c66b95fe5dcef1097362
Danial <danial-baizak@mail.ru>
2023-11-11 02:51:08 +0100

    fix

--- api/tests.py
+++ api/tests.py
@@ -55,3 +55,4 @@ class HouseAPITestCase(APITestCase, URLPatternsTestCase):
         url = reverse('get_house_info', args=["nonexistent_house"])
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
